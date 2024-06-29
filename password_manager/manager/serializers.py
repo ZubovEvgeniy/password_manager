bcc8cb3fcc6673470405e472877b13010241091e
Evgeniy Zubov <valzubof@yandex.ru>
2024-06-30 01:35:15 +0400

    add validator

--- password_manager/manager/serializers.py
+++ password_manager/manager/serializers.py
@@ -5,4 +5,10 @@ from .models import Password
 class PasswordSerializer(serializers.ModelSerializer):
     class Meta:
         model = Password
-        fields = ['service_name', 'password']
+        fields = ('service_name', 'password')
+
+    def validate(self, data):
+        if data['service_name'] == data['password']:
+            raise serializers.ValidationError(
+                'Fields must be different')
+        return data
