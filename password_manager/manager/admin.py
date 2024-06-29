581da9479a0d7205044eecbd730515343c5dcae0
Evgeniy Zubov <valzubof@yandex.ru>
2024-06-29 23:01:59 +0400

    change admin settings

--- password_manager/manager/admin.py
+++ password_manager/manager/admin.py
@@ -2,4 +2,9 @@ from django.contrib import admin
 
 from .models import Password
 
-admin.site.register(Password)
+
+class PasswordAdmin(admin.ModelAdmin):
+    list_display = ("service_name", "password")
+
+
+admin.site.register(Password, PasswordAdmin)
